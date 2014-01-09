d4b172894b04b30aac19667c870256e5c2bc9aad
Benny Ng <philoman@gmail.com>
2014-01-09 17:11:54 +0800

    ajax add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,14 +27,14 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-
+    
     respond_to do |format|
       if @comment.save
         # format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.html { redirect_to @book, notice: 'Comment was successfully created.' }
         format.json { render action: 'show', status: :created, location: @comment }
       else
-        format.html { render action: 'new' }
+        format.html { render json: @comment }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
